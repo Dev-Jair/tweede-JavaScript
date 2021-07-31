540fcc66e3e0b94e2927ab7d5aa62ed031806aa7
Dev-Jair <86578182+Dev-Jair@users.noreply.github.com>
2021-08-01 00:08:24 +0200

    Add files via upload

---
+++ W1D2-3/script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy studenten");
+// Dit is een grote som
+/* let sum = 1230941 * 1823794;
+console.log(sum)
+let sum1 = 637263 / 54;
+console.log(sum1) */
+// Vraag 4: Ik zie de uitkomst van mijn comment niet in de console staan. Dit omdat comments ervoor zorgen dat de code of tekst die erin wordt aangegeven niet wordt uitgevoerd.
+// Vraag 6: De uitkomst van de sommen is niet meer zichtbaar in de console van node. Doordat beide sommen in de blok comment vallen, word deze code niet uitgevoerd.
